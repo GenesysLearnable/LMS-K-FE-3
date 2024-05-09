fcddba25aebc976782bdea3f656a8bed29fb3010
derajsp <DeraJSP@gmail.com>
2024-05-09 11:26:54 +0100

    finished react router setup

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import "./App.css";
 import LandingPage from "./pages/landing_page/LandingPage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./Login";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <>
         <Routes>
           <Route exact path="/" element={<LandingPage />}></Route>{" "}
+          <Route exact path="/Login" element={<Login />}></Route>{" "}
         </Routes>
       </>
     </Router>
