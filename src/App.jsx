72759774aab123cfc3305c0a5ccbba744fc556ae
catherineozonnadi <catherineozonnadi@gmail.com>
2024-05-12 11:01:52 -0800

    added how old is amaka's page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
-import Amaka from './assets/components/amaka'
+// import Amaka from './assets/components/amaka'
+import Age from './assets/components/age'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-  <Amaka />
+  {/* <Amaka /> */}
+  <Age />
    
     </>
   )
