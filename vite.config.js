c3182bc9fdb52abc02311b575ca6ebadefcd3d4d
DeraJSP <derajsp@gmail.com>
2024-05-13 05:33:52 +0100

    edited package.json

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+
+export default defineConfig({ base: "/lms-k-fe-3", plugins: [react()] });
